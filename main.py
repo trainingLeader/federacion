abedbe8fe42a4620ba8d19e715550920bf6a0409
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-01-18 17:03:57 -0500

    caommi 2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from tabulate import tabulate
 os.system('cls')
-equipos = [['A',9],['B',2],['c',1],['d',0]]
+equipos = [['A',9],['B',2],['c',1],['d',0]] 
 orden = equipos.sort()
 for i,item in enumerate(equipos):
     for j in range(int(i+1),len(equipos),1):
